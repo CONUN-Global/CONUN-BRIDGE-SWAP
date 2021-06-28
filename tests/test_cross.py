53992cc4bf032173022c5b848a5fd0035263043e
Aziiiz <asirojiddinov96@gmail.com>
2021-06-28 14:13:49 +0900

    test contract deposit

--- tests/test_cross.py
+++ tests/test_cross.py
@@ -4,40 +4,36 @@ from brownie import Wei, reverts, chain
 
 LOGGER = logging.getLogger(__name__)
 
-LOCKED_AMOUNT = 100e18
+AMOUNT = 100e18
 
 
-def test_cross(accounts, projecttoken, locker):
+def test_cross(accounts, projecttoken, bridge):
         with reverts("Please approve first"):
-            locker.lockTokens(
-                projecttoken.address,
-                LOCKED_AMOUNT,
-                chain.time() + 100,
+            bridge.depositTokens(
+                AMOUNT,
+                1,
                 accounts[0],
                 {'from': accounts[0]}
             )
-        logging.info(accounts[1])
-        projecttoken.approve(locker.address, projecttoken.balanceOf(accounts[0]), {'from': accounts[0]})
-        locker.lockTokens(
-                projecttoken.address,
-                LOCKED_AMOUNT,
-                chain.time() + 100,
-                accounts[1],
+        projecttoken.approve(bridge.address, projecttoken.balanceOf(accounts[0]), {'from': accounts[0]})
+        bridge.depositTokens(
+                AMOUNT,
+                1,
+                accounts[0],
                 {'from': accounts[0]}
             )
 
 
-        # with reverts("Please approve first"):
-        #     bettoken.burn(accounts[0], 1, {"from":accounts[1]})
-        logging.info(projecttoken.balanceOf(locker.address))
-        assert projecttoken.balanceOf(locker.address) == LOCKED_AMOUNT
+
+        logging.info(projecttoken.balanceOf(bridge.address))
+        # assert projecttoken.balanceOf(locker.address) == AMOUNT
 
 
 
 
-def test_claim(accounts, projecttoken, locker):
-    with reverts("You are not allowed to take this token"):
-        locker.claimTokens(0, {'from': accounts[0]})
-
+# def test_claim(accounts, projecttoken, locker):
+#     with reverts("You are not allowed to take this token"):
+#         locker.claimTokens(0, {'from': accounts[0]})
+#
 
 
