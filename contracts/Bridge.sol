5f5db1ff7fd2b0cfbe9c23a6f6dab409804f0d1e
Aziiiz <asirojiddinov96@gmail.com>
2021-06-28 14:12:45 +0900

    setup contract deposit

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -2,28 +2,22 @@
 import "OpenZeppelin/openzeppelin-contracts@4.1.0/contracts/access/Ownable.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.1.0/contracts/token/ERC20/IERC20.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.1.0/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./BridgeTypes.sol";
 
 pragma solidity ^0.8.2;
 
-contract Bridge is BridgeTypes, Ownable {
+contract Bridge is Ownable {
     using SafeERC20 for IERC20;
 
-    LockStorageRecord[] lockerStorage;
 
     address public CON_IERC20;
 
-    // public variables
-    uint256 totalBalance;
-
-
     // mappings
-    mapping(uint256 => mapping(address => uint256)) public withdrawals;
-    mapping(uint256 => mapping(address => uint256)) public deposits;
+    mapping(uint256 => mapping(address => uint256)) private withdrawals;
+    mapping(uint256 => mapping(address => uint256)) private deposits;
 
     // events
-    event NewDeposit(address indexed user, uint256 indexed amount);
-    event NewWithdraw(address indexed user, uint256 indexed amount, uint256 withdrawId);
+    event NewDeposit(address indexed from, uint256 indexed depositId, uint256 indexed amount);
+    event NewWithdraw(address indexed user, uint256 withdrawId, uint256 indexed amount);
 
     function depositTokens(
         uint256 _amount,
@@ -38,25 +32,16 @@ contract Bridge is BridgeTypes, Ownable {
         require(IERC20(CON_IERC20).allowance(msg.sender, address(this)) >= _amount, "Please approve first");
         require(msg.sender == user, "Depositing user should be same as msg.sender");
 
-
-
-
+        // store amount
+        deposits[depositId][msg.sender] = _amount;
 
         IERC20 token = IERC20(CON_IERC20);
         token.safeTransferFrom(msg.sender, address(this), _amount);
 
+        emit NewDeposit(msg.sender, depositId, _amount);
+
     }
 
-
-
-
-    function getLockRecordByIndex(uint256 _index) external view returns (LockStorageRecord memory){
-        return _getLockRecordByIndex(_index);
-    }
-
-    function getLockCount() external view returns (uint256) {
-        return lockerStorage.length;
-    }
 
 
 
@@ -64,15 +49,24 @@ contract Bridge is BridgeTypes, Ownable {
     /////////// Only Owner           ////////////////////////////
     ////////////////////////////////////////////////////////////
 
-    function claimTokens(uint256 _amount) external onlyOwner {
+    function claimTokens(
+        uint256 _amount,
+        address _to, uint256
+        withdrawId
+    )
+        external onlyOwner
+    {
         //Lets get our lockRecord by index
+        require(_amount <= IERC20(CON_IERC20).balanceOf(address(this)), "Insufficient balance");
+        require(_to != address(0), "sender address must be valid address");
 
-        require(_amount <= totalBalance, "Insufficient balance");
 
-
-
+        withdrawals[withdrawId][_to] = _amount;
         //send tokens
-        CON_IERC20.safeTransfer(msg.sender, lock.amount);
+        IERC20 token = IERC20(CON_IERC20);
+        token.safeTransfer(_to, _amount);
+
+        emit NewWithdraw(_to, withdrawId, _amount);
     }
 
 
@@ -82,17 +76,4 @@ contract Bridge is BridgeTypes, Ownable {
 
         CON_IERC20 = conun;
     }
-
-
-
-    ////////////////////////////////////////////////////////////
-    /////////// Internals           ////////////////////////////
-    ////////////////////////////////////////////////////////////
-
-
-
-    function _getLockRecordByIndex(uint256 _index) internal view returns (LockStorageRecord memory){
-        return lockerStorage[_index];
-    }
-
 }
